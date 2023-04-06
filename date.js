cb452c265ab069ec2d151abc66334b12d34236bc
Michael Fiumarelli <106891260+mfium@users.noreply.github.com>
2023-04-06 10:56:21 -0500

    Add all the files used to create this.
    All of the node modules left out, they will be downloaded when you initialize node on your environment. best Michael

---
+++ date.js
@@ -0,0 +1,16 @@
+exports.getDate = function () {
+
+    let today = new Date();
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    return day = today.toLocaleDateString("en-US", options);
+
+
+
+}
+
